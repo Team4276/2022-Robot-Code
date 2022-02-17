d68ffba47b3eda822efd45b5e4e082c6279d8bb1
AndyC <andrew.cappon@gmail.com>
2022-02-17 11:52:01 -0800

    Once a second might be too slow to be useful.  Increased checking RGB to 5/second, should be plenty given the speed of ball handling in the intake

--- RGBsensorTest/src/main/java/frc/systems/RgbSensorRunnable.java
+++ RGBsensorTest/src/main/java/frc/systems/RgbSensorRunnable.java
@@ -92,7 +92,7 @@ public class RgbSensorRunnable implements Runnable {
             }
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(200);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
