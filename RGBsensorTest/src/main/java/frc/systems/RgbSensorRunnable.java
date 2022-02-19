23a67d2559c999e098ffd96d5fa4c3c9b6b66cc4
Peter V <pavanvooren100@student.hbuhsd.edu>
2022-02-19 13:10:06 -0800

    Modified logic to display color of ball correctly on smartdashboard. Lowered delay to increase accuracy.

--- RGBsensorTest/src/main/java/frc/systems/RgbSensorRunnable.java
+++ RGBsensorTest/src/main/java/frc/systems/RgbSensorRunnable.java
@@ -11,12 +11,24 @@ package frc.systems;
 import frc.utilities.TCS34725_I2C;
 import frc.utilities.TCS34725_I2C.TransferAbortedException;
 
+
+
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RgbSensorRunnable implements Runnable {
+    public enum COLOR {
+        FAIL, RED, BLUE, NONE
+    };
 
     public Boolean isSensorOK = true;
+    public static COLOR currentColor;
+    public static TCS34725_I2C.TCSColor myTcsColor = new TCS34725_I2C.TCSColor(0, 0, 0, 0);
+    public static double scaledRed;
+    public static double scaledGreen;
+    public static double scaledBlue;
+    public static double redThreshold = 2.1450;
+    public static double blueThreshold = 1.6766;
 
     private TCS34725_I2C myRGB;
     private final int CHANGE_DELAY = 5; // Get the same error or OK this many times in a row before changing
@@ -25,11 +37,6 @@ public class RgbSensorRunnable implements Runnable {
 
     @Override
     public void run() {
-
-        SmartDashboard.putNumber("RGB-C", -1);
-        SmartDashboard.putNumber("RGB-R", -1);
-        SmartDashboard.putNumber("RGB-G", -1);
-        SmartDashboard.putNumber("RGB-B", -1);
 
         myRGB = new TCS34725_I2C(I2C.Port.kOnboard, false);
         try {
@@ -50,9 +57,7 @@ public class RgbSensorRunnable implements Runnable {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        TCS34725_I2C.TCSColor myTcsColor = new TCS34725_I2C.TCSColor(0, 0, 0, 0);
-
+        // will get rgb data and check if sensor is working
         while (true) {
 
             try {
@@ -66,7 +71,7 @@ public class RgbSensorRunnable implements Runnable {
                     } else {
                         isSensorOK = true;
                     }
-                } 
+                }
             } catch (TransferAbortedException e) {
                 if (!isSensorOK) {
                     errorStatusChangeDelay = CHANGE_DELAY;
@@ -80,19 +85,37 @@ public class RgbSensorRunnable implements Runnable {
             }
 
             if (!isSensorOK) {
-                SmartDashboard.putNumber("RGB-C", -1);
-                SmartDashboard.putNumber("RGB-R", -1);
-                SmartDashboard.putNumber("RGB-G", -1);
-                SmartDashboard.putNumber("RGB-B", -1);
+                //if sensor is having problems, then it will set the fail state to true
+                //It will then only display fail
+                SmartDashboard.putBoolean("FAIL", true);
+                SmartDashboard.putBoolean("BLUE", false);
+                SmartDashboard.putBoolean("RED", false);
+                
             } else {
-                SmartDashboard.putNumber("RGB-C", myTcsColor.getC());
-                SmartDashboard.putNumber("RGB-R", myTcsColor.getR());
-                SmartDashboard.putNumber("RGB-G", myTcsColor.getG());
-                SmartDashboard.putNumber("RGB-B", myTcsColor.getB());
+                //scales values to be appropriate for comparing
+                scaledRed = myTcsColor.getR() * 0.65;
+                scaledGreen = myTcsColor.getG() * 0.85;
+                scaledBlue = myTcsColor.getB();
+                //compares red and blue modifiers to see which is more prominent
+                //then, sets the current color to that color
+                if ((scaledRed / scaledBlue) > redThreshold) {
+                    currentColor = COLOR.RED;
+                } else if ((scaledBlue / scaledRed) > blueThreshold) {
+                    currentColor = COLOR.BLUE;
+                } else {
+                    currentColor = COLOR.NONE;
+                }
+                // sets inital values to false, before setting the current color to true
+                SmartDashboard.putBoolean("FAIL", false);
+                SmartDashboard.putBoolean("BLUE", false);
+                SmartDashboard.putBoolean("RED", false);
+                SmartDashboard.putBoolean(currentColor.toString(), true);
+                
             }
 
             try {
-                Thread.sleep(200);
+                //delay on how often it reads the color
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
