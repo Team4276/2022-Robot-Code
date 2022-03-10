ebb1a718b0e290668cff7ecc1c6062a6c210ff1b
Peter V <pavanvooren100@student.hbuhsd.edu>
2022-03-09 17:47:48 -0800

    Add Climber.java

--- master/src/main/java/frc/utilities/RoboRioPorts.java
+++ master/src/main/java/frc/utilities/RoboRioPorts.java
@@ -13,7 +13,7 @@ public class RoboRioPorts {
 	public static final int CAN_DRIVE_R1 = 10;
 	public static final int CAN_DRIVE_R2 = 19;
 	public static final int CAN_DRIVE_R3 = 14;
-	public static final int CAN_DRIVE_L1 = 0;
+	public static final int CAN_DRIVE_L1 = 17;
 	public static final int CAN_DRIVE_L2 = 11;
 	public static final int CAN_DRIVE_L3 = 16;
 
