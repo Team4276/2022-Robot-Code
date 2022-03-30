d9882428cec4d140c713e99bd7bb0641eda8e3b5
Peter V <pavanvooren100@student.hbuhsd.edu>
2022-03-29 17:49:12 -0700

    Added smart dashboard update for pin position. Green means you can climb, and red means you must retract the pins first.

--- master/src/main/java/frc/systems/Climber.java
+++ master/src/main/java/frc/systems/Climber.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.utilities.RoboRioPorts;
 import frc.utilities.SoftwareTimer;
@@ -23,61 +24,59 @@ public class Climber {
 
     private SoftwareTimer latchTimer;
 
-
-    public Climber(){
+    public Climber() {
         leftClimbMotor = new VictorSPX(RoboRioPorts.CAN_LEFT_CLIMB);
         rightClimbMotor = new VictorSPX(RoboRioPorts.CAN_RIGHT_CLIMB);
         leftClimbMotor.setNeutralMode(NeutralMode.Brake);
         rightClimbMotor.setNeutralMode(NeutralMode.Brake);
-        climberLatchSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, RoboRioPorts.CLIMBER_LATCH_SOLENOID1,  RoboRioPorts.CLIMBER_LATCH_SOLENOID2);
+        climberLatchSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, RoboRioPorts.CLIMBER_LATCH_SOLENOID1,
+                RoboRioPorts.CLIMBER_LATCH_SOLENOID2);
         latchTimer = new SoftwareTimer();
     }
 
-    public void runClimb(){
-        //x = down
-        //y = up
+    public void runClimb() {
+        // x = down
+        // y = up
 
-        if (Robot.xboxController.getRawButton(Xbox.X)){
+        if (Robot.xboxController.getRawButton(Xbox.X)) {
             leftClimbMotor.set(ControlMode.PercentOutput, 1);
             rightClimbMotor.set(ControlMode.PercentOutput, 1);
-            //Climber go Down(Make Robot go Up)
-        } 
-        else if (Robot.xboxController.getRawButton(Xbox.Y)){
+            // Climber go Down(Make Robot go Up)
+        } else if (Robot.xboxController.getRawButton(Xbox.Y)) {
             leftClimbMotor.set(ControlMode.PercentOutput, -0.5);
             rightClimbMotor.set(ControlMode.PercentOutput, -0.5);
-  
-        }
-        else{
-            leftClimbMotor.set(ControlMode.PercentOutput, 0);
-            rightClimbMotor.set(ControlMode.PercentOutput, 0); 
-        }
-        
-        if(Robot.xboxController.getRawButton(Xbox.LB)){
 
-            if (isLatchExtended == true){
-                if(latchTimer.isExpired()){
+        } else {
+            leftClimbMotor.set(ControlMode.PercentOutput, 0);
+            rightClimbMotor.set(ControlMode.PercentOutput, 0);
+        }
+
+        if (Robot.xboxController.getRawButton(Xbox.LB)) {
+
+            if (isLatchExtended == true) {
+                if (latchTimer.isExpired()) {
                     climberLatchSolenoid.set(Value.kReverse);
                     isLatchExtended = false;
+
                     latchTimer.setTimer(0.5);
                 }
-                
-            }
-            else{
-                if (latchTimer.isExpired()){
+
+            } else {
+                if (latchTimer.isExpired()) {
                     climberLatchSolenoid.set(Value.kForward);
                     isLatchExtended = true;
                     latchTimer.setTimer(0.5);
                 }
-                
+
+            }
+            SmartDashboard.putBoolean("PINS", isLatchExtended);
+        } else {
+            try {
+                climberLatchSolenoid.set(Value.kOff);
+            } catch (Exception e) {
             }
         }
-        else
-        climberLatchSolenoid.set(Value.kOff);
 
     }
-                      
+
 }
-        
-    
-
-
