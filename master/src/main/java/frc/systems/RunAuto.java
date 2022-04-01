1951c460de28eb5a7f5fa6a5755952329a03e5c0
Peter V <pavanvooren100@student.hbuhsd.edu>
2022-04-01 11:30:40 -0700

    Added RunAuto

---
+++ master/src/main/java/frc/systems/RunAuto.java
@@ -0,0 +1,17 @@
+package frc.systems;
+
+import frc.utilities.LimitSwitch.BallState;
+
+public class RunAuto extends AutoCode {
+    public RunAuto(){
+
+    }
+
+    public void runRobotAuto(){
+        switch (true){
+
+        }
+
+        switch: s 
+    }
+}
